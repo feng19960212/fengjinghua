f76b015464200f7ddd3391e1b7ed08e5cfeca232
fengjinghua <1264396388@qq.com>
2022-09-18 13:10:18 +0800

    提交代码

---
+++ 9-15.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int imput = 0;
+//	printf("好好学习吗？（1/0）\n");
+//	scanf("%d", &imput);
+//	if (imput == 0)
+//		printf("好offer");
+//	else
+//		printf("回家卖红薯");
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int num = 1;
+//	while (num<=100)
+//	{
+//		if (num%2 = 1)
+//		{
+//		printf("%d,num");
+//
+//		}
+//	  num++;
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	for (num = 1; num <= 100; num++)
+//	{
+//		if (num % 2 == 1)
+//			printf("%d\n",num);
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		
+		printf("%d ", i);
+			i++;
+			if (i == 5)
+			break;
+	
+	}
+	return 0;
+}
